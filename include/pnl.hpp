5c1ca44d1cb59026139b0e2f2052e392561d7f78
Tiago Moherdaui <tiagomoherdaui@gmail.com>
2024-06-07 08:01:29 -0300

    Fixed bug and added Zero-checking

--- include/pnl.hpp
+++ include/pnl.hpp
@@ -7,6 +7,7 @@ namespace mnl {
     template <int d>
     struct Polynomial {
         std::unordered_map<monIndex, double> Terms;
+
         monOrder Order() const {
             monIndex maxIndex = -1;
             for (const auto& it : Terms) {
@@ -20,14 +21,25 @@ namespace mnl {
             Polynomial<d> aux;
             for (const auto& it1 : Terms)
                 for (const auto& it2 : p.Terms)
-                    aux.Terms[Poly<d>::ProductIndex(it1.first, it2.first)] += it1.second * it2.second;
+                    aux.Terms[Poly<d>::Product(it1.first, it2.first)] += it1.second * it2.second;
             Terms = aux.Terms;
+            CheckZeroes();
             return *this;
         }
         Polynomial<d>& operator+=(const Polynomial<d>& p) {
             for (const auto& it : p.Terms)
                 Terms[it.first] += it.second;
+            CheckZeroes();
             return *this;
+        }
+        void CheckZeroes() {
+            for (auto it = Terms.begin(); it != Terms.end();)
+            {
+                if (abs(it->second) < 1e-10)
+                    it = Terms.erase(it);
+                else
+                    ++it;
+            }
         }
     };
 
@@ -36,9 +48,23 @@ namespace mnl {
         Polynomial<d> result;
         for (const auto& it1 : p1.Terms)
             for (const auto& it2 : p2.Terms)
-                result.Terms[Poly<d>::ProductIndex(it1.first, it2.first)] += it1.second * it2.second;
+                result.Terms[Poly<d>::Product(it1.first, it2.first)] += it1.second * it2.second;
+        result.CheckZeroes();
         return result;
     }
+
+#define ALIASpnl(d) using pnl ##d ##D = Polynomial<d>
+    ALIASpnl(1);
+    ALIASpnl(2);
+    ALIASpnl(3);
+    ALIASpnl(4);
+    ALIASpnl(5);
+    ALIASpnl(6);
+    ALIASpnl(7);
+    ALIASpnl(8);
+    ALIASpnl(9);
+    ALIASpnl(10);
+#undef ALIASpnl
 
 }
 #endif
