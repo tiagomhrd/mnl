96dce171b9f851fc16c26de79413e443680380f3
Tiago Moherdaui <tiagomoherdaui@gmail.com>
2024-06-07 09:40:08 -0300

    Moving project files elsewhere

---
+++ include/premake5.lua
@@ -0,0 +1,41 @@
+project "mnl"
+	location ".."
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++11"
+
+	targetdir ("build/bin/" .. outputdir .. "%{prj.name}")
+	objdir ("build/bin-int/" .. outputdir .. "%{prj.name}")
+
+	files
+	{
+		"include/**.h",
+		"include/**.hpp",
+		"include/**.cpp",
+	}
+
+	includedirs
+	{
+		"include",
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		defines {
+			"MON_DEBUG"
+		}
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		defines {
+			"MON_RELEASE"
+		}
+
+	filter "configurations:Distribute"
+		runtime "Release"
+		optimize "on"
+		defines {
+			"MON_DISTRIBUTE"
+		}
